fdf982054d7b99d1446f857dee848899d52e18dc
Son Le <leson238@gmail.com>
2019-08-08 08:19:17 -0400

    Major change in App, About, ico and profile pic:

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
+import Data from './resumeData';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-// import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      resumeData: {},
+      resumeData: Data,
       e: false,
     };
-
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  getResumeData() {
-    $.ajax({
-      url: '/resumeData.json',
-      dataType: 'json',
-      cache: false,
-      // eslint-disable-next-line func-names
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error(xhr, status) {
-        if (status) this.setState({ e: true });
-      },
-    });
   }
 
   render() {
@@ -51,7 +27,6 @@ class App extends Component {
         <About data={main} />
         <Resume data={resume} />
         <Portfolio data={portfolio} />
-        {/* <Testimonials data={testimonials} /> */}
         <Contact data={main} />
         <Footer data={main} />
       </div>
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,66 +1,60 @@
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
 
-class About extends Component {
-  render() {
-    if (this.props.data) {
-      var { name } = this.props.data;
-      var profilepic = `images/${this.props.data.image}`;
-      var { bio } = this.props.data;
-      var { street } = this.props.data.address;
-      var { city } = this.props.data.address;
-      var { state } = this.props.data.address;
-      var { zip } = this.props.data.address;
-      var { phone } = this.props.data;
-      var { email } = this.props.data;
-      var resumeDownload = this.props.data.resumedownload;
-    }
-
-    return (
-      <section id="about">
-        <div className="row">
-          <div className="three columns">
-            <img className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic" />
-          </div>
-          <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
+const About = ({ data }) => {
+  const {
+    name, bio, phone, email,
+  } = data;
+  const profilepic = `images/${data.image}`;
+  const {
+    street, city, state, zip,
+  } = data.address;
+  const resumeDownload = data.resumedownload;
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img className="profile-pic" src={profilepic} alt="Son Le Profile Pic" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <span>
+                  {street}
                   <br />
-                  <span>
-                    {street}
-                    <br />
-                    {city}
-                    {' '}
-                    {state}
+                  {city}
+                  {' '}
+                  {state}
 ,
-                    {' '}
-                    {zip}
-                  </span>
-                  <br />
-                  <span>{phone}</span>
-                  <br />
-                  <span>{email}</span>
-                </p>
-              </div>
-              <div className="columns download">
-                <p>
-                  <a href={resumeDownload} className="button">
-                    <i className="fa fa-download" />
-                    Download Resume
-                  </a>
-                </p>
-              </div>
+                  {' '}
+                  {zip}
+                </span>
+                <br />
+                <span>{phone}</span>
+                <br />
+                <span>{email}</span>
+              </p>
+            </div>
+            <div className="columns download">
+              <p>
+                <a href={resumeDownload} className="button">
+                  <i className="fa fa-download" />
+                  Download Resume
+                </a>
+              </p>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
-
+      </div>
+    </section>
+  );
+};
 export default About;
