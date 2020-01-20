3dfc659652b00cf008f139a4438faf2bbf6180f8
leson238 <leson238@gmail.com>
2020-01-19 21:53:12 -0500

    Updates

---
+++ precache-manifest.87830e4c0f3dd5dca2e0291925ffb6cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8954fa16997467f1e8c79715f8efec9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "040870d278995e72b3a0",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "82a8c7a8af8780afb6ff",
+    "url": "/static/js/2.46e08e21.chunk.js"
+  },
+  {
+    "revision": "040870d278995e72b3a0",
+    "url": "/static/js/main.50a667bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
