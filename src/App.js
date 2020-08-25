9692607480c4c3c3d816f68e7beb0ce3de1671bb
leson238 <leson238@gmail.com>
2020-08-25 15:54:36 -0400

    regular maintenance: - delete unused section - update dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Data from './resumeData';
 import './App.css';
@@ -10,7 +10,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume/Resume';
-import Contact from './Components/Contact';
+// import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio/Portfolio';
 
 class App extends Component {
@@ -21,29 +21,30 @@ class App extends Component {
       e: false,
     };
   }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-    axios({
-      method: 'POST',
-      url: 'http://localhost:3002/send',
-      data: {
-        name,
-        email,
-        messsage: message,
-      },
-    }).then((response) => {
-      if (response.data.msg === 'success') {
-        document.getElementById('contact-form').reset();
-      } else if (response.data.msg === 'fail') {
-        // eslint-disable-next-line no-alert
-        alert('Message failed to send.');
-      }
-    });
-  }
+  // Contact handling
+  
+  // handleSubmit(e) {
+  //   e.preventDefault();
+  //   const name = document.getElementById('name').value;
+  //   const email = document.getElementById('email').value;
+  //   const message = document.getElementById('message').value;
+  //   axios({
+  //     method: 'POST',
+  //     url: 'http://localhost:3002/send',
+  //     data: {
+  //       name,
+  //       email,
+  //       messsage: message,
+  //     },
+  //   }).then((response) => {
+  //     if (response.data.msg === 'success') {
+  //       document.getElementById('contact-form').reset();
+  //     } else if (response.data.msg === 'fail') {
+  //       // eslint-disable-next-line no-alert
+  //       alert('Message failed to send.');
+  //     }
+  //   });
+  // }
 
   render() {
     const { main, resume, portfolio } = this.state.resumeData;
@@ -53,7 +54,7 @@ class App extends Component {
         <About data={main} />
         <Resume data={resume} />
         <Portfolio data={portfolio} />
-        <Contact data={main} handleSubmit={this.handleSubmit} />
+        {/* <Contact data={main} handleSubmit={this.handleSubmit} /> */}
         <Footer data={main} />
       </div>
     ) : (
